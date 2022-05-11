c2ba34905bd3de46c3468f6c47cee552ebece99a
Luis Alvarez <mlpa16.2@gmail.com>
2022-05-12 01:03:29 +0800

    copied stuff from sample

--- philomain.cpp
+++ philomain.cpp
@@ -12,7 +12,17 @@ struct chopsticks{
 int main(){
   const int count = 5;
   
-  auto eat = [](chopsticks &left, chopsticks &right, int id);
+  auto eat = [](chopsticks &left, chopsticks &right, int id){
+    unique_lock<mutex> llock(left_chopstics.mu);
+    unique_lock<mutex> rlock(right_chopstics.mu);
+    
+    cout << "Philosopher " << philosopher_number << " is eating" << endl;
+    
+    chrono::milliseconds timeout(1500);
+    this_thread::sleep_for(timeout);
+    
+    cout << "Philosopher " << philosopher_number << " has finished eating" << endl;
+  }
 
 
 }
